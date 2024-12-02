d379b73ef2e9b3ddb35b419c1364a6536c57ab68
dash <daobandoso@est.utn.ac.cr>
2024-12-01 23:14:16 -0600

    deseo terminar esta mierda ya

---
+++ Log/login.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Log;
+
+/**
+ *
+ * @author dashs
+ */
+public class login {
+    private int id;
+    private String nombre;
+    private String correo;
+    private String pass;
+
+    public login() {
+    }
+
+    public login(int id, String nombre, String correo, String pass) {
+        this.id = id;
+        this.nombre = nombre;
+        this.correo = correo;
+        this.pass = pass;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+    
+    
+}
